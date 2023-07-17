bde71fead987399329894dfe579dac490544914f
Rattana <rattana.pheak.rp@gmail.com>
2023-07-17 15:22:36 +0200

    Update Composant Security
    Update Composant Security

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/inscription', name: 'inscription')]
+    public function index(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if($userForm->isSubmitted() && $userForm->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('security/inscription.html.twig', [
+            'controller_name' => 'SecurityController',
+            'form' => $userForm->createView()
+        ]);
+    }
+}
