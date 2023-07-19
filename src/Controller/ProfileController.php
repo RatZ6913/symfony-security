06fa711f61bb89364bd4b78287706b0f0a121d4e
Rattana <rattana.pheak.rp@gmail.com>
2023-07-19 13:25:56 +0200

    Contrôle d'accès part 2
    Contrôle d'accès part 2

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(): Response
     {
+
+        // $user = $this->getUser();
+        // dump($user);
+
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            // 'user' => $user
         ]);
     }
 }
+
